667015bc676ce4dffa176e1e4683aee97ee10b3a
robson <nosbor84@gmail.com>
2017-08-10 18:14:09 -0300

    Ntp module. Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# == Class: puppet_ntp
+#
+class puppet_ntp(
+  $ntp_conf = "#Managed by puppet\n server 10.20.1.2 iburst\n driftfile /var/lib/ntp/drift"
+  ) {
+  package { 'ntp':
+    ensure => installed,
+  }
+
+  file { '/etc/ntp.conf':
+    ensure  => file,
+    content => $ntp_conf,
+    owner   => 'wheel',
+    mode    => '0664',
+  }
+
+  service { 'ntpd':
+    ensure => running,
+    enable => true,
+  }
+}
