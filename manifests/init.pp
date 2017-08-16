8d28c84601e3583d1f7c59bde906a48059969e21
robson <nosbor84@gmail.com>
2017-08-16 20:13:16 -0300

    type of params declared

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 # == Class: puppet_ntp
 #
 class puppet_ntp (
-  $service_name = $::puppet_ntp::params::service_name,
-  $group        = $::puppet_ntp::params::group,
-  $conf_file    = $::puppet_ntp::params::conf_file,
-  $package_name = $::puppet_ntp::params::package_name,
+  String $service_name = $::puppet_ntp::params::service_name,
+  String $group        = $::puppet_ntp::params::group,
+  String $conf_file    = $::puppet_ntp::params::conf_file,
+  String $package_name = $::puppet_ntp::params::package_name,
 ) inherits puppet_ntp::params {
 
   include puppet_ntp::install
