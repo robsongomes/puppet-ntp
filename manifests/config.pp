d0985534e6537a4673baebdae337b2320bff453a
robson <nosbor84@gmail.com>
2017-08-20 13:38:42 -0300

    testando erb templates

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,8 @@ class puppet_ntp::config inherits puppet_ntp {
 
   file { $conf_file:
     ensure  => file,
-    content => epp('puppet_ntp/ntp.conf.epp',{'servers' => $puppet_ntp::servers, 'monitor' => $puppet_ntp::monitor}),
+    # content => epp('puppet_ntp/ntp.conf.epp',{'servers' => $puppet_ntp::servers, 'monitor' => $puppet_ntp::monitor}),
+    content => template('puppet_ntp/ntp.conf.erb'),
     owner   => 'root',
     group   => $group,
     mode    => '0664',
